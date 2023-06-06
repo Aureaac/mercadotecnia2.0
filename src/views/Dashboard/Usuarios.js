a3b9e7df438db5fd96a08e9ad6eba94413f7292b
ozuna16 <80979535+ozuna16@users.noreply.github.com>
2023-06-06 10:19:49 -0600

    ver solicitud

--- src/views/Dashboard/Usuarios.js
+++ src/views/Dashboard/Usuarios.js
@@ -9,7 +9,18 @@ import {
     Tr,
     Button,
     MdBuild,
-    useColorModeValue
+    useColorModeValue,
+    useDisclosure,
+    Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
   } from "@chakra-ui/react";
   // Custom components
   import Card from "components/Card/Card.js";
@@ -17,15 +28,21 @@ import {
   import CardHeader from "components/Card/CardHeader.js";
   import TablesUsersRow from "components/Tables/TablesUsersRow";
   import React from "react";
+  import { useEffect,useRef } from 'react';
+  import Select from 'react-select'
+
   import { tablaUsuarios, tablesTableData } from "variables/general";
   
   function Usuarios() {
     const textColor = useColorModeValue("gray.700", "white");
     const borderColor = useColorModeValue("gray.200", "gray.600");
-  
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const initialRef = useRef(null)
+    const finalRef = useRef(null)
+    const [size, setSize] = React.useState('md')
     return (
       <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
-        
+        <Button onClick={onOpen}>Agregar nuevo usuario</Button>
         <Card
           my="22px"
           overflowX={{ sm: "scroll", xl: "hidden" }}
@@ -78,7 +95,108 @@ import {
             </Table>
           </CardBody>
         </Card>
-  
+        <Modal
+                    initialFocusRef={initialRef}
+                    finalFocusRef={finalRef}
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    size='lg'
+                >
+                    <ModalOverlay />
+                    <ModalContent>
+                    <ModalHeader>Alta de usuarios </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <FormControl>
+                                        <FormLabel>Nombre</FormLabel>
+                                        <Input ref={initialRef} placeholder='Nombre' />
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-6">
+                                    <FormControl >
+                                        <FormLabel>Apellido paterno</FormLabel>
+                                        <Input placeholder='Apellido paterno' />
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-6">
+                                    <FormControl mt={4}>
+                                        <FormLabel>Apellido materno</FormLabel>
+                                        <Input placeholder='Apellido materno' />
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-6">
+                                    <FormControl mt={4}>
+                                        <FormLabel>Correo</FormLabel>
+                                        <Input placeholder='Correo' />
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-12">
+                                    <FormControl mt={4}>
+                                        <FormLabel>Sede</FormLabel>
+                                        <Select placeholder='SELECCIONA UNA OPCIÓN'>
+                                        <option value='option1'>Querétaro</option>
+                                        <option value='option2'>Ciudad de México</option>
+                                        <option value='option3'>León</option>
+                                        </Select>
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-12">
+                                    <FormControl mt={4}>
+                                        <FormLabel>Tipo de usuario</FormLabel>
+                                        <Select placeholder='SELECCIONA UNA OPCIÓN'>
+                                        <option value='option1'>Director área</option>
+                                        <option value='option2'>Ejecutivo de mercadotecnia</option>
+                                        <option value='option3'>Capturista</option>
+                                        </Select>
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-12">
+                                    <FormControl mt={4}>
+                                        <FormLabel>Área</FormLabel>
+                                        <Select placeholder='SELECCIONA UNA OPCIÓN'>
+                                        <option value='option1'>ADMINMISTRACIÓN</option>
+                                        <option value='option2'>SCIO</option>
+                                        <option value='option3'>VENTAS</option>
+                                        </Select>
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-6">
+                                    <FormControl mt={4}>
+                                        <FormLabel>Áreas</FormLabel>
+                                        <Input placeholder='' disabled />
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-6">
+                                    <FormControl mt={4}>
+                                        <FormLabel>Usuario</FormLabel>
+                                        <Input placeholder='Usuario'  />
+                                    </FormControl>
+                                </div>
+                                <div className="col-lg-6">
+                                    <FormControl mt={4}>
+                                        <FormLabel>Contraseña</FormLabel>
+                                        <Input placeholder='Contraseña'  />
+                                    </FormControl>
+                                </div>
+
+                                
+                            
+                            </div>
+                        </div>
+                        
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3}>
+                        Guardar
+                        </Button>
+                        <Button onClick={onClose}>Cancelar</Button>
+                    </ModalFooter>
+                    </ModalContent>
+                </Modal>
   
    
       </Flex>
